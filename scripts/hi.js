ec366c580f56b105f547f5828ff1c8d0fbcbb105
Katya Safford <katya@Chirons-MacBook-Air.local>
2016-08-08 19:02:16 -0500

    print spotify url

--- scripts/hi.js
+++ scripts/hi.js
@@ -4,6 +4,8 @@
 
 //after git push - DO git push heroku master
 
+const https = require('https');
+
 module.exports = function(robot) {
   //when anyone types 'hi' bot will respong
   robot.hear(/hi/i, function(msg) {
@@ -17,6 +19,22 @@ module.exports = function(robot) {
 
   robot.respond(/hello/i, function(msg) {
     msg.send("http://38ccda.medialib.glogster.com/media/650b48a16f03fe794640a00af0b4453a284d0ba0d6cce58e9c599fc5394447a0/big-bend-picture2.jpg");
+  });
+
+  robot.respond(/nirvana/i, function(msg) {
+    https.get('https://api.spotify.com/v1/search?q=nirvana&type=artist', function(response) {
+        // Continuously update stream with data
+        var body = '';
+        response.on('data', function(d) {
+            body += d;
+        });
+        response.on('end', function() {
+
+            // Data reception is done, do whatever with it!
+            var parsed = JSON.parse(body);
+            msg.send(parsed.artists.items[0].external_urls.spotify);
+        });
+    });
   });
 
 //to look for help: run bin/hubot and then type Mr Bot help
