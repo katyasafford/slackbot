9aa612ca12c90854df3a3c11ed924c7a19098e3a
Katya Safford <katya@Chirons-MacBook-Air.local>
2016-08-14 16:20:50 -0500

    slackbot

--- scripts/hi.js
+++ scripts/hi.js
@@ -2,17 +2,18 @@
 // hi - Hubot responds with 'hi from hubot!'
 //@hubot bye - Hubot responds with 'bye bye from hubot!'
 
+//to make code work push it and
 //after git push - DO git push heroku master
 
 const https = require('https');
 
 module.exports = function(robot) {
-  //when anyone types 'hi' bot will respong
+  //when anyone types 'hi' bot will respond
   robot.hear(/hi/i, function(msg) {
     msg.send("hi from hubot!");
   });
 
-  //when people mention @slackbot and type bye, only then he will respond
+  //you have to mention @slackbot and type 'bye'
   robot.respond(/bye/i, function(msg) {
     msg.send("bye bye from hubot!");
   });
@@ -21,9 +22,8 @@ module.exports = function(robot) {
     msg.send("http://38ccda.medialib.glogster.com/media/650b48a16f03fe794640a00af0b4453a284d0ba0d6cce58e9c599fc5394447a0/big-bend-picture2.jpg");
   });
 
-  robot.respond(/spotify play (.*)/i, function(msg) {
-    console.log(msg.match);
-    //https.get('https://api.spotify.com/v1/search?q=nirvana&type=artist', function(response) {
+  robot.respond(/play artist (.*)/i, function(msg) {
+    //console.log(msg.match);
     var artist = msg.match[1];
       https.get('https://api.spotify.com/v1/search?q=' + artist + '&type=artist', function(response) {
         // Continuously update stream with data
@@ -32,10 +32,52 @@ module.exports = function(robot) {
             body += d;
         });
         response.on('end', function() {
-
             // Data reception is done, do whatever with it!
             var parsed = JSON.parse(body);
-            msg.send(parsed.artists.items[0].external_urls.spotify);
+            if (parsed.artists.items[0] == undefined) {
+              msg.send("Artist not found");
+            }
+            else {
+              msg.send(parsed.artists.items[0].external_urls.spotify);
+            }
+        });
+    });
+  });
+
+  robot.respond(/play album (.*)/i, function(msg) {
+    var album = msg.match[1];
+      https.get('https://api.spotify.com/v1/search?q=' + album + '&type=album', function(response) {
+        var body = '';
+        response.on('data', function(d) {
+            body += d;
+        });
+        response.on('end', function() {
+            var parsed = JSON.parse(body);
+            if (parsed.albums.items[0] == undefined) {
+              msg.send("Album not found");
+            }
+            else {
+              msg.send(parsed.albums.items[0].external_urls.spotify);
+            }
+        });
+    });
+  });
+
+  robot.respond(/play track (.*)/i, function(msg) {
+    var track = msg.match[1];
+      https.get('https://api.spotify.com/v1/search?q=' + track + '&type=track', function(response) {
+        var body = '';
+        response.on('data', function(d) {
+            body += d;
+        });
+        response.on('end', function() {
+            var parsed = JSON.parse(body);
+            if (parsed.tracks.items[0] == undefined) {
+              msg.send("Track not found");
+            }
+            else {
+              msg.send(parsed.tracks.items[0].external_urls.spotify);
+            }
         });
     });
   });
--- scripts/spotify.js
+++ scripts/spotify.js
@@ -4,7 +4,8 @@ const https = require('https');
 
 
 
-    https.get('https://api.spotify.com/v1/search?q=nirvana&type=artist', function(response) {
+    https.get('https://api.spotify.com/v1/search?q=' + "crazy train" + '&type=track', function(response) {
+
         // Continuously update stream with data
         var body = '';
         response.on('data', function(d) {
@@ -14,6 +15,15 @@ const https = require('https');
 
             // Data reception is done, do whatever with it!
             var parsed = JSON.parse(body);
-            console.log(parsed.artists.items[0].external_urls.spotify);
+
+            //console.log(parsed);
+
+            if (parsed.tracks.items[0] == undefined) {
+              console.log("Not found");
+            }
+            //console.log(parsed.artists.items[0].external_urls.spotify);
+            else {
+              console.log(parsed.tracks.items[0].external_urls.spotify);
+            }
         });
     });
